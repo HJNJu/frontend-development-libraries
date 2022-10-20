afabb8f26002ff71d1410954649af537090bcc92
Hyojin <hyojin.ju@gmx.net>
2022-10-20 15:56:43 +0200

    First jsx practice

---
+++ react/jsx/index.js
@@ -0,0 +1,23 @@
+const h1 = document.createElement("h1")
+h1.textContent = "Hello, React!"
+h1.className = "header"
+console.log(h1)
+// <h1 class="header">
+
+
+const element = <h1 className="header">This is JSX</h1>
+console.log(element)
+/*
+ {
+    type: "h1", 
+    key: nill, 
+    ref: null, 
+    props: {className: "header", children: "This is JSX"},
+    _owner: null, 
+    _store: {}
+ }
+*/
+
+
+//JSX
+ReactDOM.render(element, document.getElementById("root"))
